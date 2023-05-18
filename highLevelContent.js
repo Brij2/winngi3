72e116dba8a6b32832583a046ceb45528a11246b
Brij2 <brij4268@gmail.com>
2023-05-18 10:41:17 +0530

    pagesAdd

--- highLevelContent.js
+++ highLevelContent.js
@@ -9,7 +9,7 @@ function dynamicClothingSection(ob) {
 
   let boxLink = document.createElement("a");
   // boxLink.href = '#'
-  boxLink.href = "/contentDetails.html?" + ob.id;
+  boxLink.href = "/newindex1.html";
   // console.log('link=>' + boxLink);
 
   let imgTag = document.createElement("img");
@@ -24,20 +24,20 @@ function dynamicClothingSection(ob) {
   let h3Text = document.createTextNode(ob.name);
   h3.appendChild(h3Text);
 
-  let h4 = document.createElement("h4");
-  let h4Text = document.createTextNode(ob.brand);
-  h4.appendChild(h4Text);
+//   let h4 = document.createElement("h4");
+//   let h4Text = document.createTextNode(ob.brand);
+//   h4.appendChild(h4Text);
 
-  let h2 = document.createElement("h2");
-  let h2Text = document.createTextNode("rs  " + ob.price);
-  h2.appendChild(h2Text);
+//   let h2 = document.createElement("h2");
+//   let h2Text = document.createTextNode("rs  " + ob.price); // Price
+//   h2.appendChild(h2Text);
 
   boxDiv.appendChild(boxLink);
   boxLink.appendChild(imgTag);
   boxLink.appendChild(detailsDiv);
   detailsDiv.appendChild(h3);
-  detailsDiv.appendChild(h4);
-  detailsDiv.appendChild(h2);
+//   detailsDiv.appendChild(h4);
+//   detailsDiv.appendChild(h2);
 
   return boxDiv;
 }
